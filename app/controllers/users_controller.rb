3386b9df90a80aa27da8cac6689ad927ba746960
LorandLorincz <slimboyfat_l@yahoo.com>
2013-05-01 17:46:59 +0300

    Finish user edit, update, index and delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,     only: [:edit, :update]
+
   def new
     @user = User.new
   end
   def show
     @user = User.find(params[:id])
+  end
+  def edit
+  end
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Successfully updated..."
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 def create
     @user = User.new(params[:user])
@@ -12,7 +27,29 @@ def create
       flash[:success] = "Successfully created..."
       redirect_to @user
     else
-      render 'new'
+      render 'show'
     end
   end
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User deleted"
+  redirect_to users_url
+end
+  private
+
+      def signed_in_user
+        unless signed_in?
+          store_location
+          redirect_to signin_url, notice: "Please sign in"
+        end
+      end
+
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_path) unless current_user?(@user)
+      end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -23,12 +23,35 @@ describe "AuthenticationPages" do
         fill_in "Email", with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign in"
+        cookies[:remember_token] = user.remember_token
       end
       it { page.should have_selector('title', :text=> user.name) }
       it { page.should have_link('Profile', :href=> user_path(user)) }
+      it { page.should  have_link('Settings', :href=> edit_user_path(user)) }
       it { page.should  have_link('Sign out', :href=> signout_path) }
       it { page.should_not have_link('Sign in', :href=> signin_path) }
     end
 
   end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+  end
+
 end
